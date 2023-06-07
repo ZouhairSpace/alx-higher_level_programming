c86dd81e11fbef4149db34374702e74160ec1d2b
ZouhairSpace <jeddi.zouhair@gmail.com>
2023-06-06 19:01:46 -0700

    t12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
 def fizzbuzz():
-    output = []
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            output.append("FizzBuzz")
-        elif i % 5 == 0:
-            output.append("Buzz")
-        elif i % 3 == 0:
-            output.append("Fizz")
+    result = ""
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            result += "FizzBuzz "
+        elif number % 3 == 0:
+            result += "Fizz "
+        elif number % 5 == 0:
+            result += "Buzz "
         else:
-            output.append(str(i))
-    print(' '.join(output))
+            result += "{} ".format(number)
+    print(result)
 
 fizzbuzz()
