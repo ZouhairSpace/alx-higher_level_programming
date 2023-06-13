69ba0208fcc80fb85a75ef9aa8d3b7de8fd02e6d
ZouhairSpace <jeddi.zouhair@gmail.com>
2023-06-12 19:17:57 -0700

    t6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(input_matrix=[[]]):
-    if input_matrix != [[]]:
-        for row in input_matrix:
+def print_matrix_integer(matrix=[[]]):
+    if matrix != [[]]:
+        for row in matrix:
             for element in row:
                 print("{:d}".format(element), end=" " if element != row[-1] else '\n')
     else:
