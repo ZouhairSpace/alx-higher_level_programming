df6f126d05869bc80901e2c72684460d2fa64ffa
ZouhairSpace <jeddi.zouhair@gmail.com>
2023-06-14 08:34:07 -0700

    t0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum of two numbers."""
+    from add_0 import add
+
+    num1 = 1
+    num2 = 2
+    print("{} + {} = {}".format(num1, num2, add(num1, num2)))
